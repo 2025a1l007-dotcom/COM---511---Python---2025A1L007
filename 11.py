b063768041c6da8c959bbc5be9349efe1def1761
2025a1l007-dotcom <2025a1l007@mietjammu.in>
2026-09-13 13:35:07 +0530

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,7 @@
+#write a python program to replace double spaces from problem 3 with single spaces# Program to replace double spaces with single spaces
+
+text = input("Enter a string: ")
+
+text = text.replace("  ", " ")
+
+print("Updated string:", text)""
