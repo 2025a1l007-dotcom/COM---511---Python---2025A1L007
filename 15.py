599247043ee9983f405e5c59c7560c1939cf8a08
2025a1l007-dotcom <2025a1l007@mietjammu.in>
2026-09-13 13:37:40 +0530

    Add files via upload

---
+++ 15.py
@@ -0,0 +1,4 @@
+#write a python program to take an email address and print the domain name.
+email = input("Enter email address: ")
+
+print (email.endswitch("mietjammu.in"))
---
+++ 16.py
@@ -0,0 +1,4 @@
+#write a python program to check wheather an email ends with miet.jammuin
+email = input("Enter email address: ")
+
+print (email.endswitch("mietjammu.in"))
