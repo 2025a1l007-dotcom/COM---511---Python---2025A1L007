9813d9f520d479e36d0ebdca788e7708cd0df5ec
2025a1l007-dotcom <2025a1l007@mietjammu.in>
2026-09-13 13:39:49 +0530

    Add files via upload

---
+++ 22.py
@@ -0,0 +1,6 @@
+#write a python program to take  first name AND LAST name and print intials.
+first_name = input("Enter first name: ")
+last_name = input("Enter last name:  ")
+
+print("Intials:", first_name[0].upper() +
+      last_name[0].upper())
